0144468ccb41a2d84019810c443a6e80a84febe0
johnqing <csssnow@gmail.com>
2014-07-02 00:07:11 +0800

    add: login

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,49 @@
+var util = require('../lib/util');
+
+/**
+ * 检测登录状态
+ * @type {{notLogin: Function, login: Function}}
+ */
+var loginChect = {
+    notLogin: function(req, res, next){
+        if(req.session.user){
+            req.flash('error', '已经登录!');
+            return res.redirect('/admin');
+        }
+        next();
+    },
+    login: function(req, res, next){
+        if(!req.session.user){
+            req.flash('error', '未登录!');
+            return res.redirect('/admin/login');
+        }
+        next();
+    }
+};
+/**
+ * admin
+ * @param  {[type]} req  [description]
+ * @param  {[type]} res  [description]
+ * @param  {[type]} data [description]
+ * @return {[type]}      [description]
+ */
+function adminRender(req, res, data){
+	data = util.extend({
+		title:'主页',
+		layout: 'admin/base',
+		success: req.flash('success').toString(),
+		error: req.flash('error').toString()
+	}, data);
+
+	res.render('admin/'+data.template, data);
+}
+
+
 module.exports = function(app){
-
-	app.get('/', function(req, res){
-		res.render('index', {
-			title:'主页'
-		});
-	});
-
-
-
+	app.get('/admin', loginChect.notLogin);
 	app.get('/admin', function(req, res){
-		res.render('admin/index', {
-			title:'主页',
-			layout: 'admin/base',
-            // user: req.session.user,
-            success: req.flash('success').toString(),
-            error: req.flash('error').toString()
+		adminRender(req, res, {
+			template: 'index'
 		});
 	});
 }
